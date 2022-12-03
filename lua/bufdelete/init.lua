382f73c058bd76730a9e0356e86dadb0192f290c
Chance <chanceusc@gmail.com>
2022-12-03 11:41:40 -0500

    update comment

--- lua/bufdelete/init.lua
+++ lua/bufdelete/init.lua
@@ -55,7 +55,7 @@ local function buf_kill(range, force, wipeout)
 
                 if choice == 's' or choice == 'S' then  -- Save changes to the buffer.
                     api.nvim_buf_call(bufnr, function() cmd.write() end)
-                elseif choice ~= 'i' and choice ~= 'I' then  -- If not ignored, do not close
+                elseif choice ~= 'i' and choice ~= 'I' then  -- If not ignored, remove buffer from targets.
                     target_buffers[bufnr] = nil
                 end
 
