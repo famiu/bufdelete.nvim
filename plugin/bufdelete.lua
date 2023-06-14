3d348162a16015836cd316ac9f3c9e700e15fad5
Famiu Haque <famiuhaque@proton.me>
2023-06-14 15:09:40 +0600

    fix: correctly parse command bar

--- plugin/bufdelete.lua
+++ plugin/bufdelete.lua
@@ -2,11 +2,11 @@
 vim.api.nvim_create_user_command(
     'Bdelete',
     function(opts) require('bufdelete')._buf_kill_cmd(opts, false) end,
-    { bang = true, count = true, addr = 'buffers', nargs = '?' }
+    { bang = true, bar = true, count = true, addr = 'buffers', nargs = '?' }
 )
 
 vim.api.nvim_create_user_command(
     'Bwipeout',
     function(opts) require('bufdelete')._buf_kill_cmd(opts, true) end,
-    { bang = true, count = true, addr = 'buffers', nargs = '?' }
+    { bang = true, bar = true, count = true, addr = 'buffers', nargs = '?' }
 )
