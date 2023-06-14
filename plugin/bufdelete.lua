94774b1746db72c82e3f36632d142a361ed09000
Famiu Haque <famiuhaque@proton.me>
2023-06-14 19:58:15 +0600

    feat: add buffer completion

--- plugin/bufdelete.lua
+++ plugin/bufdelete.lua
@@ -2,11 +2,25 @@
 vim.api.nvim_create_user_command(
     'Bdelete',
     function(opts) require('bufdelete')._buf_kill_cmd(opts, false) end,
-    { bang = true, bar = true, count = true, addr = 'buffers', nargs = '*' }
+    {
+        bang = true,
+        bar = true,
+        count = true,
+        addr = 'buffers',
+        nargs = '*',
+        complete = 'buffer'
+    }
 )
 
 vim.api.nvim_create_user_command(
     'Bwipeout',
     function(opts) require('bufdelete')._buf_kill_cmd(opts, true) end,
-    { bang = true, bar = true, count = true, addr = 'buffers', nargs = '*' }
+    {
+        bang = true,
+        bar = true,
+        count = true,
+        addr = 'buffers',
+        nargs = '*',
+        complete = 'buffer'
+    }
 )
